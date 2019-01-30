09cc35db2147068463b614ca6b571365e36253c5
adithak <adithak59@gmail.com>
2019-01-30 14:44:55 +0530

    Create qq.py

---
+++ qq.py
@@ -0,0 +1,6 @@
+sm1=input("Enter first string:")
+sm2=input("Enter second string:")
+if(sorted(sm1)==sorted(sm2)):
+      print("The strings are anagrams.")
+else:
+      print("The strings aren't anagrams.")
